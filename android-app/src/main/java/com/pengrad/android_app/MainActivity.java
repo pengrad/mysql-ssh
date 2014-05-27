e62b9b93304d6d1999f95527bdf3c622eb0ab2e7
Stas Parshin <pengrad@bk.ru>
2014-05-27 23:59:44 +0400

    Main Actovoty

---
+++ android-app/src/main/java/com/pengrad/android_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pengrad.android_app;
+
+import android.support.v7.app.ActionBarActivity;
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * User: stas
+ * Date: 27.05.14 23:55
+ */
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends ActionBarActivity {
+}
