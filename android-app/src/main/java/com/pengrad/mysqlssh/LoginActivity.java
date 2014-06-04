91a968d7479673f1dafb0d2bead254b7ebeb0928
Stas Parshin <pengrad@bk.ru>
2014-06-04 15:57:05 +0400

    Rename LoginActivity

--- android-app/src/main/java/com/pengrad/mysqlssh/LoginActivity.java
+++ android-app/src/main/java/com/pengrad/mysqlssh/LoginActivity.java
@@ -16,7 +16,7 @@ import android.widget.TextView;
  * Activity which displays a login screen to the user, offering registration as
  * well.
  */
-public class LoginActivity_auto extends Activity {
+public class LoginActivity extends Activity {
     /**
      * A dummy authentication store containing known user names and passwords.
      * TODO: remove after connecting to a real authentication system.
