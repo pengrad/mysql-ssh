84b50c3cd6e49546f788758704e9c0c696d508ce
Stas Parshin <pengrad@bk.ru>
2014-06-11 20:58:10 +0400

    QueryExecutor

---
+++ core/src/main/java/com/pengrad/mysqlssh/core/QueryExecutor.java
@@ -0,0 +1,16 @@
+package com.pengrad.mysqlssh.core;
+
+import java.sql.Statement;
+
+/**
+ * User: stas
+ * Date: 11.06.14 20:46
+ */
+
+public class QueryExecutor {
+
+    public QueryResult execute(Statement statement, String query) {
+        return null;
+    }
+
+}
