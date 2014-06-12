e402df43c1f5b232b611c7d45be816bd41728aab
Stas Parshin <pengrad@bk.ru>
2014-06-12 23:58:29 +0400

    QueryExecutor

--- core/src/main/java/com/pengrad/mysqlssh/core/QueryExecutor.java
+++ core/src/main/java/com/pengrad/mysqlssh/core/QueryExecutor.java
@@ -1,6 +1,11 @@
 package com.pengrad.mysqlssh.core;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * User: stas
@@ -9,8 +14,34 @@ import java.sql.Statement;
 
 public class QueryExecutor {
 
-    public QueryResult execute(Statement statement, String query) {
-        return null;
+    public QueryResult execute(Statement statement, String query) throws SQLException {
+        boolean res = statement.execute(query);
+        QueryResult queryResult = new QueryResult();
+        if (res) {
+            queryResult.setResultType(QueryResult.RESULT_TYPE_RESULTSET);
+            ResultSet resultSet = statement.getResultSet();
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            ArrayList<String> columnNames = new ArrayList<>();
+            for (int i = 1; i <= columnCount; i++) {
+                columnNames.add(metaData.getColumnName(i));
+            }
+            queryResult.setColumnNames(columnNames);
+
+            List<List<Object>> data = new ArrayList<>();
+            while (resultSet.next()) {
+                ArrayList<Object> row = new ArrayList<>(columnCount);
+                for (int i = 1; i <= columnCount; i++) {
+                    row.add(resultSet.getObject(i));
+                }
+                data.add(row);
+            }
+            queryResult.setResultSet(data);
+        } else {
+
+        }
+        return queryResult;
     }
 
 }
