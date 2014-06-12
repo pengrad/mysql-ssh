264e95180dfbfc311f7ed948b23129bcc7d81e33
Stas Parshin <pengrad@bk.ru>
2014-06-13 00:00:02 +0400

    QueryResult

--- core/src/main/java/com/pengrad/mysqlssh/core/QueryResult.java
+++ core/src/main/java/com/pengrad/mysqlssh/core/QueryResult.java
@@ -1,6 +1,7 @@
 package com.pengrad.mysqlssh.core;
 
-import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * User: stas
@@ -11,13 +12,49 @@ public class QueryResult {
 
     public static final int RESULT_TYPE_RESULTSET = 1;
     public static final int RESULT_TYPE_ROWCOUNT = 2;
+    public static final int RESULT_TYPE_NONE = 3;
+
+    private int resultType;
+    private int rowCount;
+    private List<String> columnNames;
+    private List<List<Object>> resultSet;
+
+    public QueryResult() {
+        resultType = RESULT_TYPE_NONE;
+        rowCount = 0;
+        columnNames = new ArrayList<>(0);
+        resultSet = new ArrayList<>(0);
+    }
 
     public int getResultType() {
-        return RESULT_TYPE_RESULTSET;
+        return resultType;
     }
 
-    public ResultSet getResultSet() {
-        return null;
+    public void setResultType(int resultType) {
+        this.resultType = resultType;
     }
 
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public void setRowCount(int rowCount) {
+        this.rowCount = rowCount;
+    }
+
+    public List<String> getColumnNames() {
+        return columnNames;
+    }
+
+    public void setColumnNames(List<String> columnNames) {
+        this.columnNames = columnNames;
+    }
+
+    public List<List<Object>> getResultSet() {
+        return resultSet;
+    }
+
+    public void setResultSet(List<List<Object>> resultSet) {
+        this.resultSet = resultSet;
+    }
 }
